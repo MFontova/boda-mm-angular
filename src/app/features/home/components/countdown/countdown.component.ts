42047f0c828cff931b1dbc461e0b4083a97a0f8f
mfontova <mfontova@synergie.es>
2024-11-25 18:04:36 +0100

    countdown and images

--- src/app/features/home/components/countdown/countdown.component.ts
+++ src/app/features/home/components/countdown/countdown.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { diffDays } from '@formkit/tempo'
 
 @Component({
   selector: 'app-countdown',
@@ -9,11 +10,13 @@ import { Component } from '@angular/core';
 export class CountdownComponent {
   today: Date
   weddingDate: Date
+  remainingDays: number
 
   constructor() {
     this.today = new Date();
     this.today.setHours(0, 0, 0, 0);
     this.weddingDate = new Date(2025, 5, 6);
+    this.remainingDays = diffDays(this.weddingDate, this.today);
   }
   
 }
